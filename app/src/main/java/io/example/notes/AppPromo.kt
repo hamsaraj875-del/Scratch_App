5d065c73d23198df1ff226ee24b5e83d8a2a850c
Hamsaraj VC <hamsaraj875@gmail.com>
2026-09-17 19:36:21 +0530

    initial commit

---
+++ app/src/main/java/io/example/notes/AppPromo.kt
@@ -0,0 +1,34 @@
+package io.example.notes
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AppPromo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_app_promo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val getStartedButton: Button;
+        getStartedButton = findViewById(R.id.getStartedButton);
+
+        getStartedButton.setOnClickListener{
+            loginActivity();
+        }
+    }
+
+    fun loginActivity(){
+        val intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent);
+    }
+}
